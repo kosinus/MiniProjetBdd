18eeb377e5f4263094c02277ad9a6ac1210cdb3f
Petracca <petracca@petracca-desktop.(none)>
2012-02-25 13:26:19 +0100

    include des fonctions html

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+require_once("fonctions_fichier_html.php");
+html_entete_page('Page principale','style.css','script.js');
+?>
+
+<h2>Voici la page principale</h2>
+
+
+<?php
+html_fin_page();
+?>
