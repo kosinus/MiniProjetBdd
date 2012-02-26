9886b4d55d3ad97bc7a04760e9b4a073028ea8ce
Petracca <petracca@petracca-desktop.(none)>
2012-02-26 13:16:43 +0100

    modification OK

--- ville.php
+++ ville.php
@@ -9,8 +9,20 @@ if($_GET['ajout']==1){
 	$nomVille=$_POST['newNom'];
 	$add=mysql_query("INSERT INTO VILLE VALUES (null,'$nomVille')",$link);
 }
+/*applique la modification*/
+if($_GET['modif']==1){
+	$nomVille=$_POST['modNom'];
+	$mod=mysql_query("UPDATE VILLE SET nomVille='".$nomVille."' WHERE noVille='".$_GET['no']."'",$link);
+}
+if($_GET['action']==2){
+	$editNo=$_GET['no'];
+	echo "<form id=\"formVille\" method=\"post\" action=\"ville.php?modif=1&no=$editNo\">\n";
+}
+else{
+	echo "<form id=\"formVille\" method=\"post\" action=\"ville.php?ajout=1\">\n";
+	$editNo=-1;
+}
 ?>
-<form id="formVille" method="post" action="ville.php?ajout=1">
 <table border=1 id=tabVille>
 	<caption>
 		Villes Participantes
@@ -28,20 +40,29 @@ while($l=mysql_fetch_array($res0)){
 	echo"<tr>\n";
 	/*No Ville*/
 	echo"		<td><input type=\"text\"disabled=\"disabled\"value=\"$l[0]\"/></td>\n";
-	/*Nom Ville*/
-	echo"		<td><input type=\"text\"disabled=\"disabled\"value=\"$l[1]\"/></td>\n";
-	/*Boutons (liens)*/
-	echo"		<td>\n
-						<a
-							href=\"ville.php?action=2&no=$l[0]\"/>
-							<img src=\"ressources/ico_edit.gif\"/>
-						</a>\n
-						<a
-								href=\"ville.php?action=3&no=$l[0]\"
-								onclick=\"return(confirm('Les clubs et les athlètes de cette ville seront supprimés ! Etes-vous sûr(e) ?'));\"/>
-								<img src=\"ressources/ico_delete.gif\"/>
-						</a>\n
-					</td>\n";
+	if($l[0]==$editNo){
+		/*Nom Ville*/
+		echo"		<td><input name=\"modNom\"type=\"text\"value=\"$l[1]\"/></td>\n";
+		/*Boutons (submit)*/
+		echo"<td><input type=\"image\" src=\"ressources/ico_ok.gif\" alt=\"valider\"/></td>\n";
+	}
+	else {
+		/*Nom Ville*/
+		echo"		<td><input type=\"text\"disabled=\"disabled\"value=\"$l[1]\"/></td>\n";
+		/*Boutons (liens)*/
+		echo"		<td>\n
+							<a
+								href=\"ville.php?action=2&no=$l[0]\"/>
+								<img src=\"ressources/ico_edit.gif\"/>
+							</a>\n
+							<a
+									href=\"ville.php?action=3&no=$l[0]\"
+									onclick=\"return(confirm('Les clubs et les athlètes de cette ville seront supprimés ! Etes-vous sûr(e) ?'));\"/>
+									<img src=\"ressources/ico_delete.gif\"/>
+							</a>\n
+						</td>\n";
+	}
+	echo"</tr>\n";
 }
 /*barre d'ajout*/
 if($_GET['action']==1){
