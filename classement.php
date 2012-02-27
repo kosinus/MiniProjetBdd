3a7018a65c03fe9499a8f8762e80b4cd42ac9f76
Petracca <petracca@petracca-desktop.(none)>
2012-02-27 14:51:53 +0100

    preparation de la page

--- classement.php
+++ classement.php
@@ -3,6 +3,28 @@ require_once("main.php");
 ?>
 
 
+<form method="post" id="formClassement" action="classement.php">
+<table border=1>
+	<caption>Classement</caption>
+	<thead>
+		<tr>
+			<th>Compétition</th>
+			<th>Discipline</th>
+			<th>Classement</th>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+
+?>
+	</tbody>
+</table>
+<a href="classement.php"><img src="ressources/ico_add2.png"/></a>
+</form>
+
+
+
+
 <?php
 html_fin_page();
 ?>
