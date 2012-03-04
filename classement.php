744fb00eb4d79fffc75863e9d06c02dba8637695
Petracca <petracca@petracca-desktop.(none)>
2012-03-04 22:32:06 +0100

    insertion + suppression OK

--- classement.php
+++ classement.php
@@ -1,9 +1,18 @@
 <?php
 require_once("main.php");
+/*suppression*/
+if($_GET['action']==3)
+	$sup=mysql_query("delete from CLASSEMENT where noCompet=".$_GET['comp']." and noDiscipline=".$_GET['dis']." and noAthlete=".$_GET['ath'],$link);
+/*insertion*/
+if($_GET['action']==1){
+	$noAthlete=$_POST['noAthlete'];
+	$noDiscipline=$_POST['noDiscipline'];
+	$noCompet=$_POST['noCompet'];
+	$classement=$_POST['classement'];
+	$add=mysql_query("INSERT INTO CLASSEMENT VALUES ($noAthlete,$noCompet,$noDiscipline,$classement)",$link);
+}
 ?>
-
-
-<form method="post" id="formClassement" action="classement.php">
+<form method="post" id="formClassement" action="classement.php?action=1">
 <table border=1 style="position:absolute;left:5%;right:5%">
 	<caption>Classement</caption>
 	<thead>
@@ -69,16 +78,54 @@ while($lcomp=mysql_fetch_array($comp)){
 			else
 				echo 		"<div id=\"notfirst\">".$i."e - ".$lpart[0]."</div>";
 			echo "	</td>\n";
+			echo "		<td>\n
+								<a
+									href=\"classement.php?action=2&no=$l[0]\"/>
+									<img src=\"ressources/ico_edit.gif\"/>
+								</a>\n";
+$r=mysql_query("select noCompet from COMPETITION where nomCompet='".$lcomp[0]."'",$link);
+$nocom=mysql_fetch_array($r);
+$r=mysql_query("select noDiscipline from DISCIPLINE where libelleDiscipline='".$ldis[0]."'",$link);
+$nodis=mysql_fetch_array($r);
+$r=mysql_query("select noAthlete from ATHLETE where nomAthlete='".$lpart[0]."'",$link);
+$noath=mysql_fetch_array($r);
+			echo "		<a
+									href=\"classement.php?action=3&comp=".$nocom[0]."&dis=".$nodis[0]."&ath=".$noath[0]."\"
+									onclick=\"return(confirm('Etes-vous sûr de vouloir supprimer cette entrée?'));\"/>
+									<img src=\"ressources/ico_delete.gif\"/>
+								</a>\n
+							</td>\n";
 			echo "</tr>\n";
 			$i++;
 		}
 	}
 }
+/*barre d'ajout*/
+if($_GET['ajout']==1){
+	echo "<tr>\n";
+	echo "<td><select name=\"noCompet\"><option selected>Choisissez la compétition</option>";
+	$res=mysql_query("select * from COMPETITION",$link);
+	while($l=mysql_fetch_array($res))
+		echo "<option value=\"".$l[0]."\">".$l[1]."</option>";
+	echo "</select></td>\n";
+	echo "<td><select name=\"noDiscipline\"><option selected>Choisissez la discipline</option>";
+	$res=mysql_query("select * from DISCIPLINE",$link);
+	while($l=mysql_fetch_array($res))
+		echo "<option value=\"".$l[0]."\">".$l[1]."</option>";
+	echo "</select></td>\n";
+	echo "<td><select name=\"noAthlete\"><option selected>Choisissez l'athlete</option>";
+	$res=mysql_query("select * from ATHLETE",$link);
+	while($l=mysql_fetch_array($res))
+		echo "<option value=\"".$l[0]."\">".$l[1]."</option>";
+	echo "</select>\n";
+	echo "<input type=\"text\"name=\"classement\" /></td>\n";
+	echo"<td><input type=\"image\" src=\"ressources/ico_ok.gif\" alt=\"valider\"/></td>\n";
+	echo "</tr>\n";
+}
 ?>
-
 	</tbody>
 </table>
-<a href="classement.php"><img src="ressources/ico_add2.png"style="position:absolute;bottom:5%;right:5%"/></a>
+<a href="classement.php?ajout=1"><img src="ressources/ico_add2.png"style="position:absolute;bottom:5%;right:5%"/></a>
 </form>
 
 
